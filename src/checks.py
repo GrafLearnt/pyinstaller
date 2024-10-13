43a7cf5c914efebfa6f56bef4e23c3767a511dcc
Kostiantyn <graflearnt@gmail.com>
2024-10-13 21:31:42 +0300

    reset

--- src/checks.py
+++ src/checks.py
@@ -10,25 +10,25 @@ upload_exe_with_name = env('upload_exe_with_name')
 
 
 """ Checks """
-# If required inputs missing -> ERROR
-# if not spec:
-#     set_annotation(
-#         "Required input missing: 'spec'.",
-#         'Input-Error',
-#         'error'
-#     )
+If required inputs missing -> ERROR
+if not spec:
+    set_annotation(
+        "Required input missing: 'spec'.",
+        'Input-Error',
+        'error'
+    )
 
-# If passed inputs are not supported -> ERROR
-# supported_spec = (
-#     '.py',
-#     '.spec'
-# )
-# if not spec.endswith(supported_spec):
-#     set_annotation(
-#         f"Unsupported input 'spec = {spec}' was provided. Supported types: {', '.join(supported_spec)}",
-#         'Input-Error',
-#         'error'
-#     )
+If passed inputs are not supported -> ERROR
+supported_spec = (
+    '.py',
+    '.spec'
+)
+if not spec.endswith(supported_spec):
+    set_annotation(
+        f"Unsupported input 'spec = {spec}' was provided. Supported types: {', '.join(supported_spec)}",
+        'Input-Error',
+        'error'
+    )
 
 # If useful optional arguments missing -> NOTICE
 if not upload_exe_with_name:
